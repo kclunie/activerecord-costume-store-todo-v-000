66bfd0fb94bf021d50f386c63c6576aa03bbd488
Kristen Clunie <kcluniesa@yahoo.com>
2019-03-05 02:14:43 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,11 +5,11 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.string :costume_inventory
+      t.integer :costume_inventory
       t.integer :num_of_employees
-      t.string :still_in_business
-      t.string :url
-      t.timestamps
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
