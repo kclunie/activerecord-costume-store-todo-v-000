859ef03e774e6f544e6e9ef124c7ef00cf4937a2
Kristen Clunie <kcluniesa@yahoo.com>
2019-03-05 01:50:08 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,8 @@ class CostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume do |t|
       t.string :name
+      t.string :location
+      t.string :costume_inventory
       t.integer :price
       t.integer :size 
       t.string :imageurl
