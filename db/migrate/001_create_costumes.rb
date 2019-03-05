4d912a63f78253b83b525567fb3b27519787a02a
Kristen Clunie <kcluniesa@yahoo.com>
2019-03-05 01:41:30 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,10 +10,10 @@
 
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
-    create_table :candies do |t|
-      t.string :name
-      t.integer :calories
-      t.timestamps
-    end
+    #create_table :costume do |t|
+     # t.string :name
+      #t.integer :calories
+      #t.timestamps
+    #end
   end
 end
