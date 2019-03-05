cd8f4c666f608621e24c3a0b1b1eb2379b7d748c
Kristen Clunie <kcluniesa@yahoo.com>
2019-03-05 02:10:53 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,7 @@
 
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
-    create_table :costume do |t|
+    create_table :costume_stores do |t|
       t.string :name
       t.string :location
       t.string :costume_inventory
