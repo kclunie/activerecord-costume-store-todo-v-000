a75ba81effa20d6c908596883c08e9335f017ac7
Kristen Clunie <kcluniesa@yahoo.com>
2019-03-05 01:52:03 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,8 +6,8 @@ class CostumeStores < ActiveRecord::Migration[4.2]
       t.string :name
       t.string :location
       t.string :costume_inventory
-      t.integer :price
-      t.integer :size 
+      t.integer :number_of_employees
+      t.string :still_in_business
       t.string :image_url
       t.timestamps
     end
